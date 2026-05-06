2e39977f6bb7bba1963d369ecede8717a1a5cb29
Cid437 <donntorres26@gmail.com>
2026-05-06 21:35:49 +0800

    Vite branch

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import './style.css';
+
+const app = document.querySelector('#app');
+
+app.innerHTML = `
+  <h1>Vite Template</h1>
+  <p>You're ready to build with Vite.</p>
+  <button id="count">Count: 0</button>
+`;
+
+const button = document.querySelector('#count');
+let count = 0;
+button.addEventListener('click', () => {
+  count += 1;
+  button.textContent = `Count: ${count}`;
+});
