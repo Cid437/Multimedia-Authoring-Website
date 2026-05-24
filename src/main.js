31b71adf7a5766ad2da80c9a9a1e7d389cf78657
Cid437 <donntorres26@gmail.com>
2026-05-24 19:24:03 +0800

    temp link fix

--- src/main.js
+++ src/main.js
@@ -1,16 +1,1 @@
 import './style.css';
-
-function attachClickById(id, href) {
-    const el = document.getElementById(id);
-
-    if (!el) return;
-
-    el.style.cursor = 'pointer';
-
-    el.addEventListener('click', () => {
-        window.location.href = href;
-    });
-}
-
-attachClickById('roto', './Portfolio/rotoscope.html');
-attachClickById('solar', './Portfolio/solarsystem.html');
